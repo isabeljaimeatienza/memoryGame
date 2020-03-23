f0d3e6142ac9e796f3dea88ea731784b54387d06
cadit <cadit@192.168.13.79>
2020-03-23 18:59:35 +0100

    Cartas colocadas con el for. 	-Colocación al azar de las cartas 	-Poder ganar 	- ERROR AL MOSTRAR EN MATRIZ(se salta una columna y fila)

--- src/main/java/es/isabeljaimeatienza/memory/Logica.java
+++ src/main/java/es/isabeljaimeatienza/memory/Logica.java
@@ -5,49 +5,71 @@
  */
 package es.isabeljaimeatienza.memory;
 
+import static es.isabeljaimeatienza.memory.Tablero.COLUMNS;
+import static es.isabeljaimeatienza.memory.Tablero.ROWS;
+
 /**
  *
  * @author cadit
  */
 public class Logica {
-   
-    char[][] cuadricula = new char[4][6];
+   static final int CARTA= 0;
+   int numeroCarta= 0;
+
+   static int[][] cuadricula = new int[ROWS][COLUMNS];
 // con dos corchetes quiere decir en 2 dimensiones, 
     //por tanto al querer una tabla ponemos 2, [6][] es 6 por 4. Se le llama a esto array o matriz
     
    // Metodo constructor que llamaremos como la clase
     //Aqui empezaremos la partida
     public Logica (){
-        for(int f=0; f<Tablero.rows; f++){
-            for(int c=0; c<Tablero.columns; c++){
-                cuadricula[f][c] ='.';
+        double numeroCarta=0;
+        for(int f=0; f<ROWS; f++){
+            for(int c=0; c<COLUMNS; c++){
+                
+                colocarCarta(c,f,((int)Math.floor(numeroCarta))); // redondeo hacia abajo con math.floor
+                cuadricula[f][c] = ((int)Math.floor(numeroCarta));
+                numeroCarta = numeroCarta+0.5;
+//               
             }
         
         }
     }
     
     public void mostrarConsola(){
-        for(int f=0; f<Tablero.rows; f++){
-            for(int c=0; c<Tablero.columns; c++){
-//                System.out.print(cuadricula[f][c]); // mostraría en consola los puntitos
+        for(int f=0; f<ROWS; f++){
+            for(int c=0; c<COLUMNS; c++){
+                System.out.print(cuadricula[f][c]); // mostraría en consola los numeros de carta en su posición
             }
-//            System.out.println("");
+            System.out.println("");
          
         }
         
     } 
-        public int getFilaColocar(int columnaColocar){
-        int filaColocar = -1; // -1  nos referimos que no se pueda colocar
-        // Mientras no hayamos llegado al final y la fila de abajo esté vacía
-        while(filaColocar<Tablero.rows && cuadricula[filaColocar+1][columnaColocar]=='.'){
-            filaColocar++;
+
+    public void colocarCarta(int fila, int columna, int carta) { //esta carta es la que guardo en parámetro
+//    System.out.println("Colocando carta en matriz: "+fila+","+columna+","+numeroCarta);
+//    cuadricula[fila][columna] = carta;
+}
+
+        public void sacarCarta(int indice){ // la carta la paso como char porque sino da error
+//        
+       
+        indice=0;
+    
+        for (int fi=0; fi<ROWS; fi++){
+                        for(int co=0; co<COLUMNS; co++){
+                            indice= 6*fi+co;
+//                           System.out.println("Columna:"+ co+"Fila:"+ fi+"indice:"+indice);
+                                   }           
+                  }
+       
+        int num=0;
+//        this.getChildren().get(0); 
         }
-//        System.out.println(filaColocar);
-//          System.out.println(filaColocar);
-        return filaColocar;
             
     }
     
 
     
-}
+
--- src/main/java/es/isabeljaimeatienza/memory/Tablero.java
+++ src/main/java/es/isabeljaimeatienza/memory/Tablero.java
@@ -6,11 +6,11 @@
 package es.isabeljaimeatienza.memory;
 
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
-import javafx.scene.text.Text;
 
 /**
  *
@@ -19,8 +19,8 @@ import javafx.scene.text.Text;
 public class Tablero extends GridPane{
   
     
-    static final int rows = 3;
-    static final int columns = 5;
+    static final int ROWS = 3;
+    static final int COLUMNS = 5;
     Logica logica = new Logica();
     
     
@@ -31,12 +31,12 @@ public class Tablero extends GridPane{
  
          //Setting size for the pane  
        
-       for(int i = 0; i < columns; i++) {
+       for(int i = 0; i < COLUMNS; i++) {
             ColumnConstraints column = new ColumnConstraints(100);
             this.getColumnConstraints().add(column);
         }
 
-        for(int i = 0; i < rows; i++) {
+        for(int i = 0; i < ROWS; i++) {
             RowConstraints row = new RowConstraints(180);
             this.getRowConstraints().add(row);
         }
@@ -58,19 +58,22 @@ public class Tablero extends GridPane{
                     System.out.println("Mouse clicked X , Y:  " + 
                         mouseEvent.getX() + " : " + mouseEvent.getY());
                     int clicX = (int) mouseEvent.getX();
+                    logica.mostrarConsola();
                     int columna = clicX / Carta.TAM_CARTA;
-                    System.out.println("Columna: " + columna);
+//                    System.out.println("Columna: " + columna);
                     
                     double numeroCarta = 0;
                     int indice = 0;
-                    for (int fi=0; fi<=rows; fi++){
-                        for(int co=0; co<=columns; co++){
+                    for (int fi=0; fi<=ROWS; fi++){
+                        for(int co=0; co<=COLUMNS; co++){
 //                           System.out.println("Columna:"+ co+"Fila:"+ fi+"Carta:" + (int)numeroCarta); 
-                           sacarCarta(co,fi,indice);
-                           indice=indice+1;
-                           colocarCarta(co,fi,(int) Math.floor(numeroCarta)); // redondeo hacia abajo con math.floor
-                           numeroCarta = numeroCarta+0.5;
-//                         
+                           
+                            colocarCarta(co,fi,((int) Math.floor(numeroCarta))); // redondeo hacia abajo con math.floor
+                            numeroCarta = numeroCarta+0.5;
+                  
+        //                         sacarCarta(indice);
+                           indice=6*fi+co;
+//                           System.out.println("indice:"+indice+"numCarta:"+numeroCarta);
                               
                         }
                     }//Columna-fila-carta
@@ -84,34 +87,31 @@ public class Tablero extends GridPane{
     
    private void colocarCarta(int columna, int fila, int numeroCarta){
        
+//       System.out.println("co:"+columna+"fi:"+fila+"numCarta:"+numeroCarta);
        Carta carta = new Carta(numeroCarta); // numCarta te dirá¡ quien da el click, viene en el parametro de este metodo el anterior da igual
-       int filaColocar;
-       filaColocar = logica.getFilaColocar(columna);
-       logica.mostrarConsola();
-//       logica.colocarCarta(columna,filaColocar, numeroCarta);
-       
-//       this.sacarCarta(numeroCarta,filaColocar,int numecarta);
-//       colocarCarta(filaColocar,columna,'1');
+
        carta.setLayoutY(Carta.TAM_CARTA/2);
+       
        this.add(carta,columna,fila);
         }
        
     // Crear método que me coja la carta que hay en una determinada posición (1,1) que se pase por parámetro
     // Guardar en variable el numero de carta que le corresponde    
 //       
-        public void sacarCarta(int filaColocar, int columna,int indi){ // la carta la paso como char porque sino da error
+    public void sacarCarta(int indice){ // la carta la paso como char porque sino da error
 //        
-        Tablero carta = null;
-        int indice=0;
-        
-        for (int fi=0; fi<=rows; fi++){
-                        for(int co=0; co<=columns; co++){
-                        
-                           System.out.println("Columna:"+ co+"Fila:"+ fi);
+       
+        indice=0;
+    
+        for (int fi=0; fi<=ROWS; fi++){
+                        for(int co=0; co<=COLUMNS; co++){
+                            indice= 6*fi+co;
+//                           System.out.println("Columna:"+ co+"Fila:"+ fi+"indice:"+indice);
                                    }           
                   }
        
-        this.getChildren().get(0); // y seria fila x seria columna
+        int num=0;
+        this.getChildren().get(0); 
         }
     }  
    
