ebd3f8a84ce638b67437c18bb783c955223836a9
cadit <cadit@192.168.13.79>
2020-03-27 14:38:47 +0100

    Logrado: 	+ Colocación aleatoria (matriz y tablero) Falta: 	-Borrar las que están debajo 	- Girar cartas 	-Lógica del juego

--- src/main/java/es/isabeljaimeatienza/memory/Logica.java
+++ src/main/java/es/isabeljaimeatienza/memory/Logica.java
@@ -7,69 +7,77 @@ package es.isabeljaimeatienza.memory;
 
 import static es.isabeljaimeatienza.memory.Tablero.COLUMNS;
 import static es.isabeljaimeatienza.memory.Tablero.ROWS;
+import java.util.Random;
 
 /**
  *
  * @author cadit
  */
 public class Logica {
-   static final int CARTA= 0;
-   int numeroCarta= 0;
 
-   static int[][] cuadricula = new int[ROWS][COLUMNS];
+    static final int CARTA = 0;
+    int numeroCarta = 0;
+
+    int[][] cuadricula = new int[ROWS][COLUMNS];
 // con dos corchetes quiere decir en 2 dimensiones, 
     //por tanto al querer una tabla ponemos 2, [6][] es 6 por 4. Se le llama a esto array o matriz
-    
-   // Metodo constructor que llamaremos como la clase
+
+    // Metodo constructor que llamaremos como la clase
     //Aqui empezaremos la partida
-    public Logica (){
-        double numeroCarta=0;
-        for(int f=0; f<ROWS; f++){
-            for(int c=0; c<COLUMNS; c++){
-                
-                colocarCarta(c,f,((int)Math.floor(numeroCarta))); // redondeo hacia abajo con math.floor
-                cuadricula[f][c] = ((int)Math.floor(numeroCarta));
-                numeroCarta = numeroCarta+0.5;
-//               
+    public Logica() {
+
+        double numeroCarta = 0;       
+        for (int f = 0; f < ROWS; f++) {
+            for (int c = 0; c < COLUMNS; c++) {
+
+                colocarCarta(c, f, ((int) Math.floor(numeroCarta))); // redondeo hacia abajo con math.floor
+                cuadricula[f][c] = ((int) Math.floor(numeroCarta));
+                numeroCarta = numeroCarta + 0.5;
+
             }
-        
+
         }
+
+        desordenarCartas();
+
     }
-    
-    public void mostrarConsola(){
-        for(int f=0; f<ROWS; f++){
-            for(int c=0; c<COLUMNS; c++){
-                System.out.print("|"+cuadricula[f][c]); // mostraría en consola los numeros de carta en su posición
+
+    public void mostrarConsola() {
+        for (int f = 0; f < ROWS; f++) {
+            for (int c = 0; c < COLUMNS; c++) {
+                System.out.print("|" + cuadricula[f][c]); // mostraría en consola los numeros de carta en su posición
             }
             System.out.println("");
-         
+
         }
-        
-    } 
+
+    }
 
     public void colocarCarta(int fila, int columna, int carta) { //esta carta es la que guardo en parámetro
 //    System.out.println("Colocando carta en matriz: "+fila+","+columna+","+numeroCarta);
 //    cuadricula[fila][columna] = carta;
-}
-
-        public void sacarCarta(int indice){ // la carta la paso como char porque sino da error
-//        
-       
-        indice=0;
-    
-        for (int fi=0; fi<ROWS; fi++){
-                        for(int co=0; co<COLUMNS; co++){
-                            indice= 6*fi+co;
-//                           System.out.println("Columna:"+ co+"Fila:"+ fi+"indice:"+indice);
-                                   }           
-                  }
-       
-        int num=0;
-//        this.getChildren().get(0); 
-        }
-            
     }
-    
 
-    
+    public void desordenarCartas() { // la carta la paso como char porque sino da error
+        //Intercambiamos la carta de la posición (0,1) con la (2,3)
 
+        Random r = new Random();
+        for (int fi = 0; fi < ROWS; fi++) {
+            for (int co = 0; co < COLUMNS; co++) {
+                int rC = r.nextInt(COLUMNS);
+                int rF = r.nextInt(ROWS);
+                int cartaOrigen = cuadricula[fi][co];
+                int cartaDestino = cuadricula[rF][rC];
+                cuadricula[fi][co] = cartaDestino;
+                cuadricula[rF][rC] = cartaOrigen;
+                
+         // En cada vuelta del bucle deberá de cambiar rF y rC. 
+
+//                            indice= 6*fi+co;
+//                           System.out.println("Columna:"+ co+"Fila:"+ fi+"indice:"+indice);
+            }
+        }
+
+    }
+
+}
--- src/main/java/es/isabeljaimeatienza/memory/Tablero.java
+++ src/main/java/es/isabeljaimeatienza/memory/Tablero.java
@@ -5,124 +5,127 @@
  */
 package es.isabeljaimeatienza.memory;
 
+import java.util.Random;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
+import javafx.geometry.Pos;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.RowConstraints;
 
 /**
  *
  * @author cadit
  */
-public class Tablero extends GridPane{
-  
-    
+public class Tablero extends GridPane {
+
     static final int ROWS = 4;
     static final int COLUMNS = 6;
     Logica logica = new Logica();
-    
-    
-    public Tablero(){
-   
- 
-       
- 
-         //Setting size for the pane  
-       
-       for(int i = 0; i < COLUMNS; i++) {
-            ColumnConstraints column = new ColumnConstraints(100);
+
+    public Tablero() {
+
+        //Setting size for the pane  
+        for (int i = 0; i < COLUMNS; i++) {
+            ColumnConstraints column = new ColumnConstraints(Carta.TAM_CARTA * 1.34);
             this.getColumnConstraints().add(column);
         }
 
-        for(int i = 0; i < ROWS; i++) {
-            RowConstraints row = new RowConstraints(180);
+        for (int i = 0; i < ROWS; i++) {
+            RowConstraints row = new RowConstraints(Carta.TAM_CARTA * 2);
             this.getRowConstraints().add(row);
         }
 
-     
-       
-        this.setStyle("-fx-background-color: WHITE ;-fx-padding: 200 ; -fx-insets-background: BLUE; -fx-grid-lines-visible: true");
-       
-        
-        
+        this.setStyle("-fx-background-color: WHITE ;-fx-padding: 200 ;-fx-insets-colspan:23; -fx-insets-background: BLUE; -fx-grid-lines-visible: true");
 
-        
-    
-               // Detectar clic en ratÃ³n (pulsado y soltado)
-           this.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent mouseEvent) {
-                    // Insertar aquÃ­ el cÃ³digo a ejecutar cuando se haga clic en el ratÃ³n
-                    System.out.println("Mouse clicked X , Y:  " + 
-                        mouseEvent.getX() + " : " + mouseEvent.getY());
-                    int clicX = (int) mouseEvent.getX();
-                    logica.mostrarConsola();
-                    int columna = clicX / Carta.TAM_CARTA;
+        // Detectar clic en ratÃ³n (pulsado y soltado)
+        this.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                // Insertar aquÃ­ el cÃ³digo a ejecutar cuando se haga clic en el ratÃ³n
+                System.out.println("Mouse clicked X , Y:  "
+                        + mouseEvent.getX() + " : " + mouseEvent.getY());
+                int clicX = (int) mouseEvent.getX();
+                logica.mostrarConsola();
+                int columna = clicX / Carta.TAM_CARTA;
 //                    System.out.println("Columna: " + columna);
-                    
-                    double numeroCarta = 0;
-                    int indice = 0;
-                    for (int fi=0; fi<ROWS; fi++){
-                        for(int co=0; co<COLUMNS; co++){
-//                           System.out.println("Columna:"+ co+"Fila:"+ fi+"Carta:" + (int)numeroCarta); 
-                           
-                            colocarCarta(co,fi,((int) Math.floor(numeroCarta))); // redondeo hacia abajo con math.floor
-                            numeroCarta = numeroCarta+0.5;
-                  
-        //                         sacarCarta(indice);
-                           indice=6*fi+co;
-//                           System.out.println("indice:"+indice+"numCarta:"+numeroCarta);
-                              
-                        }
-                    }//Columna-fila-carta
-                        
-                   
-                }
-            });
-            
-          
-    }
-    
-   private void colocarCarta(int columna, int fila, int numeroCarta){
-       
-//       System.out.println("co:"+columna+"fi:"+fila+"numCarta:"+numeroCarta);
-       Carta carta = new Carta(numeroCarta); // numCarta te dirá¡ quien da el click, viene en el parametro de este metodo el anterior da igual
 
-       carta.setLayoutY(Carta.TAM_CARTA/2);
-       
-       this.add(carta,columna,fila);
-        }
-       
+                double numeroCarta = 0;
+                int indice = 0;
+                Random r = new Random();
+                int rC = r.nextInt(11);
+                int rO = r.nextInt(11);
+                int cartaOrigen = rO;
+                int cartaDestino = rC;
+                cartaOrigen = rO;
+                cartaDestino = rC;
+                rC = cartaOrigen;
+                rO = cartaDestino;
+                for (int fi = 0; fi < ROWS; fi++) {
+                    for (int co = 0; co < COLUMNS; co++) {
+//                           System.out.println("Columna:"+ co+"Fila:"+ fi+"Carta:" + (int)numeroCarta); 
+
+//                        borrarCarta(fi, co, indice); //redondeo hacia abajo con math.floor
+                        // Consulta el valor de matriz y coloca la carta que se encuentre en ella   
+                        colocarCarta(fi, co, logica.cuadricula[fi][co]);
+
+//                           System.out.println("indice:"+indice+"numCarta:"+numeroCarta);
+                    }
+                }//Columna-fila-carta
+
+            }
+        });
+
+    }
+
+    private void colocarCarta(int fila, int columna, int numeroCarta) {
+
+//       System.out.println("co:"+columna+"fi:"+fila+"numCarta:"+numeroCarta);
+        Carta carta = new Carta(numeroCarta); // numCarta te dirá¡ quien da el click, viene en el parametro de este metodo el anterior da igual
+
+        carta.setLayoutY(Carta.TAM_CARTA / 2);
+
+        this.add(carta, columna, fila); // para quitar carta usar remove en vez de add
+        this.setAlignment(Pos.CENTER);
+    }
+
     // Crear método que me coja la carta que hay en una determinada posición (1,1) que se pase por parámetro
     // Guardar en variable el numero de carta que le corresponde    
 //       
-    public void sacarCarta(int indice){ // la carta la paso como char porque sino da error
+    public void sacarCarta(int indice) { // la carta la paso como char porque sino da error
 //        
-       
-        indice=0;
-    
-        for (int fi=0; fi<ROWS; fi++){
-                        for(int co=0; co<COLUMNS; co++){
-                            indice= 6*fi+co;
+
+        indice = 0;
+
+        for (int fi = 0; fi < ROWS; fi++) {
+            for (int co = 0; co < COLUMNS; co++) {
+                indice = 6 * fi + co;
 //                           System.out.println("Columna:"+ co+"Fila:"+ fi+"indice:"+indice);
-                                   }           
-                  }
-       
-        int num=0;
-        this.getChildren().get(2); 
-        System.out.println("Indice"+indice);
-
+            }
         }
-    }  
-   
 
-    
-        
-        
-    
-    
+        this.getChildren().get(indice);
 
-
-
+    }
+//
+//    private void borrarCarta() {
+//
+////       System.out.println("co:"+columna+"fi:"+fila+"numCarta:"+numeroCarta);
+//        indice = 0;
+//
+//        for (int fi = 0; fi < ROWS; fi++) {
+//            for (int co = 0; co < COLUMNS; co++) {
+//                indice = 6 * fi + co;
+////                           System.out.println("Columna:"+ co+"Fila:"+ fi+"indice:"+indice);
+//            }
+//        }
+//
+//        this.getChildren().get(indice);
+//
+//        this.getChildren().remove(indice); // para quitar carta usar remove en vez de add
+//        this.setAlignment(Pos.CENTER);
+//    }
+}
